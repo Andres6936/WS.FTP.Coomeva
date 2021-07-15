87f783c301edeafb3a7c197d11e47692cfda5b43
Adan <adan@maildrop.cc>
2021-07-15 09:21:06 -0500

    Post binary data

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 Send PDF: https://spin.atomicobject.com/2015/10/03/remote-pfs-node-js-express/
+curl.exe --form file="@Formato.pdf" http://localhost:8000/upload
--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const upload = multer({dest: 'uploads/'});
 
 const app = express();
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
     console.log(req.file, req.body)
+    res.send("Successful");
 })
 
 app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
     console.log(req.file, req.body)
+    res.send("Successful");
 })
 
-var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+const cpUpload = upload.fields([{name: 'avatar', maxCount: 1}, {name: 'gallery', maxCount: 8}]);
 app.post('/cool-profile', cpUpload, function (req, res, next) {
     // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
     //
