778a55bcae46fe2942f0c7d766ce25617d318668
Adan <adan@maildrop.cc>
2021-07-16 08:42:02 -0500

    Avoid problem with the Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+const localTunnel = require('localtunnel')
 const express = require('express');
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
 
 const app = express();
+
+// With this approximation avoid the problem: No 'Access-Control-Allow-Origin' [...]
+app.use(((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', "false");
+
+    // Pass to next layer of middleware
+    next();
+}))
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
@@ -12,7 +32,7 @@ app.post('/upload', upload.single('file'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
     console.log(req.file, req.body)
-    res.send("Successful");
+    res.send("Successful Upload BLOB");
 })
 
 app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
@@ -35,4 +55,8 @@ app.post('/cool-profile', cpUpload, function (req, res, next) {
 
 app.listen(8000, () => {
     console.log('Running port 8000');
-})
+});
+
+localTunnel({port: 8000}).then((tunnel => {
+    console.log(tunnel.url);
+}))
