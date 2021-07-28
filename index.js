a9d098a18622dbe466ffbba84d480d98ed14f8b5
Adan <adan@maildrop.cc>
2021-07-28 08:02:14 -0500

    Added parser of headers

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const localTunnel = require('localtunnel')
 const express = require('express');
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
@@ -8,7 +7,7 @@ const app = express();
 app.use(cors({
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Authorization, Origin, X-Requested-With, Accept",
+    allowedHeaders: "Content-Type, Authorization, Origin, X-Requested-With, Accept, Cache-Control",
     preflightContinue: true,
     optionsSuccessStatus: 204,
 }));
@@ -19,17 +18,24 @@ app.options('*', cors(), ((req, res, next) => {
     res.send('CORS Allow');
 }));
 
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('1');
+})
+
 app.post('/upload', upload.single('file'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
     console.log(req.file, req.body)
-    res.send("Successful Upload BLOB");
+
+    console.log(req.headers["taylor-line"]);
+    console.log(req.headers["taylor-body"]);
+    // console.log(req.headers["Taylor-Body"].split(';'));
+    // const bodyValues = req.headers["Taylor-Body"].split(';');
+    // console.log(bodyValues.item(bodyValues.length - 1));
+    res.send("1");
 })
 
-app.listen(8000, () => {
+app.listen(80, () => {
     console.log('Running port 8000');
 });
-
-localTunnel({port: 8000}).then((tunnel => {
-    console.log(tunnel.url);
-}))
